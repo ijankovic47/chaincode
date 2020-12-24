d1bee09694ae02551e4eea721a52a6611b91b502
Ivan <ijankovic47@gmail.com>
2020-12-24 13:56:16 +0100

    chaincode dodavanje konfiguracija polja za osobu

--- chaincode.go
+++ chaincode.go
@@ -66,6 +66,8 @@ func (s *SmartContractPrinter) Invoke(stub shim.ChaincodeStubInterface) sc.Respo
 		return s.readAllPersons(stub, args)
 	case "readCert":
 		return s.readCert(stub, args)
+	case "personAddField":
+		return s.personAddField(stub, args)
 	default:
 		return shim.Error("Invalid Smart Contract function name.")
 	}
@@ -155,6 +157,122 @@ func (s *SmartContractPrinter) readCert(APIstub shim.ChaincodeStubInterface, arg
 	buffer.WriteString(", ID " + string(id))
 	return shim.Success(buffer.Bytes())
 
+}
+func (s *SmartContractPrinter) personAddField(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+	var personIdent string = args[0]
+	logger.Critical("ADDING FIELD FOR PERSON IDENT " + personIdent)
+	logger.Critical("FIELD ARG " + args[1])
+
+	personAsBytes, _ := APIstub.GetState(args[0])
+
+	if(personAsBytes == nil) {
+		shim.Error("Person ident " + personIdent + " not found !")
+	}
+	var person Person
+	json.Unmarshal(personAsBytes, &person)
+
+	var fields []Field
+	err := json.Unmarshal([]byte(args[1]), &fields)
+
+	if(err != nil) {
+		fmt.Printf("NIJE USPEO UNMARSHALING: %s", err)
+	}
+
+	if(len(person.Fields) == 0) {
+		logger.Critical("OSOBA NEMA POLJA, UPISUJEMO !")
+		person.Fields = fields
+	} else {
+		for i, newField := range fields {
+			logger.Critical("Iter " + string(i))
+			var updatedExistingField bool = false
+			for j, personField := range person.Fields {
+				if (newField.Name == personField.Name) {
+					logger.Critical("VEC POSTOJI POLJE NAZIV: " + newField.Name + ", RADIMO UPDATE !")
+					person.Fields[j] = newField
+					updatedExistingField = true
+				}
+			}
+			if(!updatedExistingField) {
+				logger.Critical("RADIMO UNOS NOVOG POLJA NAZIV: " + newField.Name)
+				person.Fields = append(person.Fields, newField)
+			}
+		}
+	}
+	logger.Critical("CUVAMO NOVU KONFIGURACIJU POLJA !")
+	insertPersonAsBytes, _ := json.Marshal(person)
+	APIstub.PutState(person.Ident, insertPersonAsBytes)
+	return shim.Success(insertPersonAsBytes)
+
+
+	return shim.Success([]byte(fmt.Sprintf("%v", fields)))
+}
+
+func (s *SmartContractPrinter) approveAccess(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+
+	client, err := cid.New(APIstub)
+	if err != nil {
+		fmt.Printf("Error creating new Smart Contract: %s", err)
+	}
+
+	id, err := client.GetID()
+	mspId, err := client.GetMSPID()
+
+	var clientId string = mspId + id
+	var personIdent string = args[0]
+	var reqClientFabricId = args[1]
+	var fieldNames []string
+
+	_ = json.Unmarshal([]byte(args[2]), &fieldNames)
+
+	logger.Infof("Person ident is:  %d", personIdent)
+	logger.Infof("Requesting client id:  %d", reqClientFabricId)
+	logger.Infof("Field names:  %d", fieldNames)
+
+	personBytes, err := APIstub.GetState(personIdent)
+
+	if err != nil {
+		fmt.Printf("Error creating new Smart Contract: %s", err)
+	}
+	if(personBytes == nil) {
+		return shim.Error("Person " + personIdent + " not found!")
+	}
+	var person Person
+	json.Unmarshal(personBytes, &person)
+	for i, f := range person.Fields {
+		fmt.Println(i, f.Name)
+		for fni, fn := range  fieldNames {
+			fmt.Println(fni, fn)
+			var isEndorser bool = false
+			if(fn == f.Name) {
+				for eidi, eid := range  f.Endorsers {
+					fmt.Println(eidi, eid)
+					if(eid == clientId) {
+						isEndorser = true
+					}
+				}
+				if(!isEndorser) {
+					break
+				}
+
+				for vpi, vp := range f.ViewPermissions {
+					fmt.Println(vpi, vp)
+					if(vp.RequesterId == reqClientFabricId) {
+						var isEndorsmentAdded bool = false
+						for eidi, eid :=range vp.Endorsers {
+							fmt.Println(eidi, eid)
+							if(eid == clientId) {
+								isEndorser = true
+							}
+						}
+						if(!isEndorsmentAdded) {
+							vp.Endorsers = append(vp.Endorsers, clientId)
+						}
+					}
+				}
+			}
+		}
+	}
+	return shim.Error("")
 }
 
 func main() {
